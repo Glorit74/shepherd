a3112eed6deb39782f6b792767c33ed6afcaf795
Flora <laszlo.t.flora@gmail.com>
2024-01-06 20:11:00 +0100

    circle around SheepIcon

--- src/components/SheepIcon/SheepIcon.js
+++ src/components/SheepIcon/SheepIcon.js
@@ -5,8 +5,21 @@ import { GiSheep } from "react-icons/gi";
 import { COLORS } from "../../constants";
 
 function SheepIcon({ size }) {
-  return <Icon size={size} />;
+  return (
+    <Wrapper size={size}>
+      <Icon size={size} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+  border: 2px solid ${COLORS.white};
+  border-radius: 50%;
+`;
 
 const Icon = styled(GiSheep)`
   width: ${(props) => props.size || "32px"};
