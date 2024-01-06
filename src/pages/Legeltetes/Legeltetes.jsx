baa2cf506d533a1d2b3154d9e1e3a743df11f928
Flora <laszlo.t.flora@gmail.com>
2024-01-06 20:33:21 +0100

    Legeltetes with carousel

---
+++ src/pages/Legeltetes/Legeltetes.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Background from "../../components/Background";
+import SheepIcon from "../../components/SheepIcon";
+import UnstyledButton from "../../components/UnstyledButton";
+
+import { COLORS } from "../../constants";
+
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardDoubleArrowLeft } from "react-icons/md";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  //   const imageContext = require.context(
+  //     "public/pictures",
+  //     false,
+  //     /\.(png|jpe?g|svg)$/
+  //   );
+  //   const images = imageContext.keys().map(imageContext);
+
+  const images = [
+    "pictures/nyaj1.jpg",
+    "pictures/nyaj2.jpg",
+    "pictures/nyaj3.jpg",
+    "pictures/nyaj4.jpg",
+    "pictures/nyaj5.jpg",
+  ];
+
+  const goToFirst = () => setCurrentIndex(0);
+  const goToPrevious = () =>
+    setCurrentIndex((prevIndex) =>
+      prevIndex > 0 ? prevIndex - 1 : images.length - 1
+    );
+  const goToNext = () =>
+    setCurrentIndex((prevIndex) =>
+      prevIndex < images.length - 1 ? prevIndex + 1 : 0
+    );
+  const goToLast = () => setCurrentIndex(images.length - 1);
+
+  return (
+    <>
+      <Background question="Mi mindent csinál" title="a pásztor">
+        <Wrapper>
+          <SubTitle>
+            <SheepIcon size={"30px"} />
+            Legeltetés
+          </SubTitle>
+
+          <CarouselWrapper>
+            <PaginationButton onClick={goToPrevious}>
+              <MdKeyboardDoubleArrowLeft size={42} />
+            </PaginationButton>
+            <PaginationButton onClick={goToFirst}>
+              <MdKeyboardArrowLeft size={42} />
+            </PaginationButton>
+
+            <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+
+            <PaginationButton onClick={goToNext}>
+              <MdOutlineKeyboardArrowRight size={42} />
+            </PaginationButton>
+            <PaginationButton onClick={goToLast}>
+              <MdOutlineKeyboardDoubleArrowRight size={42} />
+            </PaginationButton>
+			
+          </CarouselWrapper>
+        </Wrapper>
+      </Background>
+    </>
+  );
+};
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const SubTitle = styled.h2`
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  align-items: center;
+  color: ${COLORS.white};
+  margin-bottom: 1rem;
+`;
+
+const CarouselWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  
+  img {
+	  border: 5px solid ${COLORS.white};
+    display: block;
+    width: 100%;
+	margin: 0;
+
+  }
+`;
+
+const PaginationButton = styled(UnstyledButton)`
+  color: ${COLORS.white};
+  padding: 5 10px;
+`;
+export default Carousel;
