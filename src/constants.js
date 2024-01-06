733bfd107e59cb204048258b39ab840cfbf9658e
Flora <laszlo.t.flora@gmail.com>
2024-01-06 15:21:36 +0100

    git commit -m "basic data"

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+const menu = [
+  { id: 1, label: "kihajtás, behajtás", active: true },
+  { id: 2, label: "jelölés", active: false },
+  { id: 3, label: "legeltetés", active: true },
+  { id: 4, label: "juhfejés", active: false },
+  { id: 5, label: "terelés", active: false },
+  { id: 6, label: "juhnyírás", active: false },
+  { id: 7, label: "sózás", active: false },
+  { id: 8, label: "betörés", active: false },
+  { id: 9, label: "gyógyítás", active: false },
+  { id: 10, label: "takarmányozás", active: false },
+  { id: 11, label: "etetés", active: false },
+];
+
+const COLORS = {
+  white: "hls(0deg 0% 100%)",
+};
+
+export { menu, COLORS };
